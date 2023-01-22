bd2bc150cad9d93a279d9a0623cb6aef099a724c
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-01-21 17:41:25 -0800

    Added methods

--- Assignment_1/dictionary.cpp
+++ Assignment_1/dictionary.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "dictionary.h"
+
+// convert char to it's corresponding int
+int dictionary::getHashMapValue(char c) {
+    return wordHashTable.at((c));
+};
+
+// insert new word
+bool dictionary::add(const char *wordBeingInserted) {
+
+};
+
+// find node that ends in the prefix
+dictNode* dictionary::findEndingNodeOfAStr(const char *strBeingSearched) {
+
+};
+
+// count number of words in a tree that start from a node
+void dictionary::countWordsStartingFromANode(int &count) {
+
+};
