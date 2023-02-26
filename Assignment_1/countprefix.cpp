b21e1f1e24efbf7907e6ee5561d8c29e9c1d3faa
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-02-26 14:55:28 -0800

    Updated incorrect file name handling

--- Assignment_1/countprefix.cpp
+++ Assignment_1/countprefix.cpp
@@ -42,57 +42,59 @@ int main(int argc, char **argv) {
     }
 
 
-//    // check if valid number of arguments
-//    if (argc > 3){
-//        dictNode *root = new (dictNode); // create root node and initialize pointers to nullptr
-//
-//        const char *delimiters = "\n\r !\"#$%&()*+,./0123456789:;<=>?@[\\]^`{|}~";  // separates words
-//        string line;    // stores line data
-//
-//        // reads the second command line argument
-//        ifstream addstream(argv[1]);
-//
-//        if (addstream.fail()){
-//            cout << "File could not be opened" << endl;
-//        } else {
-//
-//            // reads file line by line and adds to tree word by word
-//            while (getline(addstream, line)) {
-//                char *line_c = const_cast<char *>(line.c_str());
-//                char *word = strtok(line_c, delimiters);
-//                {
-//                    while (word != nullptr) {
-//                        root->add(word);
-//                        word = strtok(nullptr, delimiters);
-//                    }
-//                }
-//            }
-//        }
-//
-//        // reads the third command line argument
-//        ifstream countstream(argv[2]);
-//
-//        if (countstream.fail()){
-//            cout << "File could not be opened" << endl;
-//        } else {
-//
-//            // reads file line by line and counts the # of words in the tree based on prefixes supplied in file
-//            while (getline(countstream, line)) {
-//                char *line_c = const_cast<char *>(line.c_str());
-//                char *word = strtok(line_c, delimiters);
-//                {
-//                    while (word != nullptr) {
-//                        int count = 0;
-//                        dictNode *end = root->findEndingNodeOfAStr(word);
-//                        end->countWordsStartingFromANode(count);
-//                        cout << word << " " << count << endl;
-//                        word = strtok(nullptr, delimiters);
-//                    }
-//                }
-//            }
-//        }
-//
-//    } else{
-//        cout << "Invalid amount of arguments" << endl;
-//    }
+    // check if valid number of arguments
+    if (argc > 2){
+        dictNode *root = new (dictNode); // create root node and initialize pointers to nullptr
+
+        const char *delimiters = "\n\r !\"#$%&()*+,./0123456789:;<=>?@[\\]^`{|}~";  // separates words
+        string line;    // stores line data
+
+        // reads the second command line argument
+        ifstream addstream(argv[1]);
+
+        if (addstream.fail()){
+            cout << "Unable to open file <<" << argv[1] << ">>" << endl;
+            exit(EXIT_FAILURE);
+        } else {
+
+            // reads file line by line and adds to tree word by word
+            while (getline(addstream, line)) {
+                char *line_c = const_cast<char *>(line.c_str());
+                char *word = strtok(line_c, delimiters);
+                {
+                    while (word != nullptr) {
+                        root->add(word);
+                        word = strtok(nullptr, delimiters);
+                    }
+                }
+            }
+        }
+
+        // reads the third command line argument
+        ifstream countstream(argv[2]);
+
+        if (countstream.fail()){
+            cout << "Unable to open file <<" << argv[2] << ">>" << endl;
+            exit(EXIT_FAILURE);
+        } else {
+
+            // reads file line by line and counts the # of words in the tree based on prefixes supplied in file
+            while (getline(countstream, line)) {
+                char *line_c = const_cast<char *>(line.c_str());
+                char *word = strtok(line_c, delimiters);
+                {
+                    while (word != nullptr) {
+                        int count = 0;
+                        dictNode *end = root->findEndingNodeOfAStr(word);
+                        end->countWordsStartingFromANode(count);
+                        cout << word << " " << count << endl;
+                        word = strtok(nullptr, delimiters);
+                    }
+                }
+            }
+        }
+
+    } else{
+        cout << "Invalid amount of arguments" << endl;
+    }
 };
